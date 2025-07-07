1e47e9bb136750b29544a29ae9110965a0dc6dde
Chris Malek <cmalek@caltech.edu>
2025-07-06 20:21:51 -0700

    Bump version: 0.2.0 → 0.2.1

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,7 +28,7 @@ copyright: str = "Christopher Malek 2025"  # noqa: A001
 author: str = "Christopher Malek"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.2.1"
 
 
 # -- General configuration ---------------------------------------------------
